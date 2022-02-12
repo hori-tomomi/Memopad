1fe322917330af5c4c2744ffd8ccbf9dd3683b7e
TomomiHori <hr.tmm.23@icloud.com>
2022-02-12 17:02:39 +0900

    Initial Commit

---
+++ Memopad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memopad
+//
+//  Created by Tomomi Hori on 2022/02/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
